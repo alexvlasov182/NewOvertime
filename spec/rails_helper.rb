3a2c787714f97b9ea74fa765085a229575cf0bdd
Alex Vlasov <drumlife182@gmail.com>
2022-08-01 12:41:40 +0200

    Added rspec, capybara and setup homepage

---
+++ spec/rails_helper.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../config/environment', __dir__)
+
+abort('The Rails environment is running in production mode!') if Rails.env.production?
+require 'spec_helper'
+require 'rspec/rails'
+require 'capybara/rails'
+
+include Warden::Test::Helpers
+Warden.test_mode!
+
+ActiveRecord::Migration.maintain_test_schema!
+
+RSpec.configure do |config|
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.use_transactional_fixtures = false
+  config.before(:suite) { DatabaseCleaner.clean_with(:truncation) }
+  config.before { DatabaseCleaner.strategy = :transaction }
+  config.before(:each, js: true) { DatabaseCleaner.strategy = :truncation }
+  config.before { DatabaseCleaner.start }
+  config.after { DatabaseCleaner.clean }
+  config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
+end
