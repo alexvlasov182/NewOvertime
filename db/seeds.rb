a73942a8d6a3cd9fa83f86208dbe6d45556698ce
Alex Vlasov <drumlife182@gmail.com>
2022-08-10 18:02:04 +0200

    Added hours requested to post model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
-@user = User.create(email: 'drumlife182@gmail.com', password: 'drumlife', password_confirmation: 'drumlife',
-                    first_name: 'Alex', last_name: 'Vlasov')
+@user = User.create(email: 'drumlife182@gmail.com', password: 'drumlife', password_confirmation: 'drumlife', first_name: 'Alex', last_name: 'Vlasov')
 
 p '1 User created'
 
@@ -11,7 +10,7 @@ AdminUser.create(email: 'admin@test.com', password: 'asdfasdf', password_confirm
 p '1 Admin User created'
 
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
+  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
 end
 
 p '100 Posts have been created'
