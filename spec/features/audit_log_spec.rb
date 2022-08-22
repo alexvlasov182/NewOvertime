e862a28d4db93058cb4f01871cbab1100801de6a
Alex Vlasov <drumlife182@gmail.com>
2022-08-22 12:28:35 +0200

    Blocked non admin users from accessing index for audits

--- spec/features/audit_log_spec.rb
+++ spec/features/audit_log_spec.rb
@@ -18,8 +18,14 @@ describe 'AuditLog Feature' do
       expect(page).to have_content(/VLASOV, ALEX/)
     end
 
-    # xit 'cannot be accessed by non admin users' do
-    #
-    # end
+    it 'cannot be accessed by non admin users' do
+      logout(:user)
+      user = FactoryGirl.create(:user)
+      login_as(user, :scope => :user)
+
+      visit audit_logs_path
+
+      expect(current_path).to eq(root_path)
+    end
   end
 end
