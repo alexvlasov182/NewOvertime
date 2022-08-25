36667e77a785d4f783b46cc8922e660dad18dd3a
Alex Vlasov <drumlife182@gmail.com>
2022-08-25 21:51:33 +0200

    Integrated basic manager notification eamil

---
+++ app/mailers/manager_mailer.rb
@@ -0,0 +1,6 @@
+class ManagerMailer < ApplicationMailer
+  def email manager
+    @manager = manager
+    mail(to: @manager.email, subject: 'Anastasia I Love So you much you the best girl and wife in the whole world, I hope i writing without mistakes:)')
+  end
+end
