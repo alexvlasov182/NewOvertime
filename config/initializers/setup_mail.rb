6c48833a8c18d86629ea54a79b8a00e92efe36d2
Alex Vlasov <drumlife182@gmail.com>
2022-09-02 16:05:15 +0200

    Added new configuration

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,8 +1,15 @@
+#GMAIL CONFIG
+config.action_mailer.default_url_options = { :host =>'localhost:3000' }
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.perform_deliveries = true
+config.action_mailer.raise_delivery_errors = true
+config.action_mailer.default :charset => "utf-8"
+
 ActionMailer::Base.smtp_settings = {
-    :user_name => 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
-    :password => ENV['SENDGRID_API_KEY'], # This is the secret sendgrid API key which was issued during API key creation
-    :domain => 'alexvlasov-overtime.herokuapp.com',
-    :address => 'smtp.sendgrid.net',
+    :user_name => ENV['GMAIL_USERNAME'], # This is the string literal 'apikey', NOT the ID of your API key
+    :password => ENV['GMAIL_PASSWORD'], # This is the secret sendgrid API key which was issued during API key creation
+    :domain => 'gmail.com',
+    :address => 'smtp.gmail.com',
     :port => 587,
     :authentication => :plain,
     :enable_starttls_auto => true
