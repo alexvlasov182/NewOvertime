c53eb3077a824278670968c0a4b8ba4e6fd4f878
Alex Vlasov <drumlife182@gmail.com>
2022-09-02 09:54:33 +0200

    Added to production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,4 +23,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { host: 'alexvlasov-overtime.herokuapp.com' }
 end
