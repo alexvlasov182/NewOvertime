81bc6c36baf9aad93aa3b551cbd49f5cbed2a48c
Alex Vlasov <drumlife182@gmail.com>
2022-09-02 16:20:38 +0200

    Added config to gmail

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,6 +1,6 @@
 
 
-ActionMailer::Base.smtp_settings = {
+config.action_mailer.smtp_settings = {
     :user_name => ENV['GMAIL_USERNAME'], # This is the string literal 'apikey', NOT the ID of your API key
     :password => ENV['GMAIL_PASSWORD'], # This is the secret sendgrid API key which was issued during API key creation
     :domain => 'gmail.com',
