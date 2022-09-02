4b4f8f089cbbbb63e3ffcc851f366c2a2e91a3ac
Alex Vlasov <drumlife182@gmail.com>
2022-09-02 16:22:12 +0200

    Added config to gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,6 +25,16 @@ Rails.application.configure do
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  config.action_mailer.smtp_settings = {
+      :user_name => ENV['GMAIL_USERNAME'], # This is the string literal 'apikey', NOT the ID of your API key
+      :password => ENV['GMAIL_PASSWORD'], # This is the secret sendgrid API key which was issued during API key creation
+      :domain => 'gmail.com',
+      :address => 'smtp.gmail.com',
+      :port => 587,
+      :authentication => :plain,
+      :enable_starttls_auto => true
+  }
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.default_url_options = { host: 'alexvlasov-overtime.herokuapp.com' }
--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,11 +1,2 @@
 
 
-config.action_mailer.smtp_settings = {
-    :user_name => ENV['GMAIL_USERNAME'], # This is the string literal 'apikey', NOT the ID of your API key
-    :password => ENV['GMAIL_PASSWORD'], # This is the secret sendgrid API key which was issued during API key creation
-    :domain => 'gmail.com',
-    :address => 'smtp.gmail.com',
-    :port => 587,
-    :authentication => :plain,
-    :enable_starttls_auto => true
-}
